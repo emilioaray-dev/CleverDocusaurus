73cd87f2505932b81d5352912959cb85be0ff105
Celsius Aray <caray@cleveritgroup.com>
2024-04-10 12:32:01 -0400

    change url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://emilioaray-dev.github.io',
+  url: 'https://emilioaray-dev.github.io/CleverDocusaurus',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
