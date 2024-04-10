dff285b3bf27403a2c83a4de7639afd4efac6d8d
Celsius Aray <caray@cleveritgroup.com>
2024-04-10 12:36:05 -0400

    fix url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,14 +8,14 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://emilioaray-dev.github.io/CleverDocusaurus',
+  url: 'https://emilioaray-dev.github.io/CleverDocusaurus/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'CleverIt', // Usually your GitHub org/user name.
+  organizationName: 'emilioaray', // Usually your GitHub org/user name.
   projectName: 'CleverDocusaurus', // Usually your repo name.
 
   onBrokenLinks: 'throw',
